93f93453ae6e6082448243ba80844a20f6bee4d7
Bradley Gerrits <bradley@snakeware.nl>
2021-10-14 12:16:58 +0200

    Initial commit

---
+++ FitchefCrawler/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+
+namespace FitchefCrawler
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            MainAsync(args).Wait();
+        }
+
+        private static async Task MainAsync(string[] args)
+        {
+            var crawler = new FitchefCrawler<Recipe>();
+
+            await crawler.Crawl();
+        }
+    }
+}
